12c5f705a9752abb58d11dc5eb9404d8bf2b96a1
Jacob I  Perez <jacobperez91@gmail.com>
2023-01-17 09:11:29 -1000

    created model controller

---
+++ TravelDiary/Controllers/Model Controller/EntryController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryController.swift
+//  TravelDiary
+//
+//  Created by Jacob Perez on 1/17/23.
+//
+
+import Foundation
+
+class EntryController {
+    
+    let shared = EntryController()
+    
+    var entries: [Entry] = []
+    
+    func createEntry(with title: String, address: String, body: String) {
+        let entry = Entry(title: title, address: address, body: body)
+        entries.append(entry)
+    }
+    func deleteEntry(entryToDelete: Entry) {
+        guard let indexOfEntryToDelete = entries.firstIndex(of: entryToDelete) else { return }
+        entries.remove(at: indexOfEntryToDelete)
+    }
+    func updateEntry(entryToUpdate: Entry, newTitle: String, newBody: String, newAddress: String) {
+        entryToUpdate.title = newTitle
+        entryToUpdate.address = newAddress
+        entryToUpdate.body = newBody
+    }
+}
+
--- TravelDiary/Model/Entry.swift
+++ TravelDiary/Model/Entry.swift
@@ -9,13 +9,24 @@ import Foundation
 
 class Entry {
     
-    let title: String
-    let entryDate: Date
-    let address: String
+    var title: String
+    var entryDate: Date
+    var address: String
+    var body: String
     
-    init(title: String, entryDate: Date = Date(), address: String) {
+    init(title: String, entryDate: Date = Date(), address: String, body: String) {
         self.title = title
         self.entryDate = entryDate
         self.address = address
+        self.body = body
     }
+}
+extension Entry: Equatable {
+    static func == (lhs: Entry, rhs: Entry) -> Bool {
+        return lhs.title == rhs.title &&
+        lhs.body == rhs.body &&
+        lhs.address == rhs.address &&
+        lhs.entryDate == rhs.entryDate
+    }
+    
 }
